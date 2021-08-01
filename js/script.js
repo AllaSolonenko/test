998b231a298debfcd9bba1281536b9832be1f056
AllaSolonenko <61387818+AllaSolonenko@users.noreply.github.com>
2021-08-01 20:47:14 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+function changeImg() { 
+  
+  var img = document.getElementById("logo"); 
+  if (img.src === "/images/IL_makiage.png") {    
+    img.src = "/images/google_logo.svg";
+  } else  {
+    img.src = "/images/IL_makiage.png";
+  }
+  
+ 
+}
