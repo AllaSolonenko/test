7012fe6e4fe2e4cd13b2fbe68cc4208f8a0638a6
AllaSolonenko <61387818+AllaSolonenko@users.noreply.github.com>
2021-08-01 20:50:08 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@
 function changeImg() { 
   
   var img = document.getElementById("logo"); 
-  if (img.src === "/images/IL_makiage.png") {    
-    img.src = "/images/google_logo.svg";
+  if (img.src === "images/IL_makiage.png") {    
+    img.src = "images/google_logo.svg";
   } else  {
-    img.src = "/images/IL_makiage.png";
+    img.src = "images/IL_makiage.png";
   }
   
  
